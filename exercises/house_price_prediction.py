781893c1e2d57aea20e906a59a9b276cc69bde7c
Yossi <yossidoctor98@gmail.com>
2023-04-04 17:54:05 +0300

    preprocess_data cleanup

--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -10,6 +10,8 @@ from IMLearn.utils import split_train_test
 
 pio.templates.default = "simple_white"
 
+PATH = r"D:\\Google Drive\\University\\IML\\IML Ex 2\\"  # todo: remove!
+
 
 def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     """
@@ -27,20 +29,22 @@ def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     Post-processed design matrix and response vector (prices) - either as a single
     DataFrame or a Tuple[DataFrame, Series]
     """
-    irrelevant_columns = ['id', 'date', 'zipcode', 'lat', 'long', 'sqft_living15', 'sqft_lot15']
-    positive_columns = ['price', 'sqft_living', 'sqft_lot', 'yr_built', 'bathrooms', 'floors']
-    non_negative_columns = ['bedrooms', 'bathrooms', 'sqft_above', 'sqft_basement', 'yr_renovated']
-    to_int_columns = ['condition', 'view', 'grade', 'yr_built',
-                      'sqft_above', 'sqft_basement', 'yr_renovated']
-    x_df = X.drop(columns=irrelevant_columns).dropna()
+    to_be_removed_columns = ['id', 'date', 'sqft_living15', 'sqft_lot15', 'price']
+    positive_columns = ['bedrooms', 'bathrooms', 'price', 'sqft_living', 'floors', 'sqft_above',
+                        'yr_built', 'zipcode']
+    to_int_columns = ['id', 'price', 'bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot',
+                      'floors', 'waterfront', 'view', 'condition', 'grade', 'sqft_above',
+                      'sqft_basement', 'yr_built', 'yr_renovated', 'zipcode',
+                      'sqft_living15', 'sqft_lot15']
+    x_df = X.dropna()
     x_df = x_df[(x_df[positive_columns] > 0).all(axis=1)]
-    x_df = x_df[(x_df[non_negative_columns] >= 0).all(axis=1)]
-    x_df = x_df.loc[x_df['waterfront'].isin(range(2)) & x_df['view'].isin(range(5)) &
-                    x_df['condition'].isin(range(6)) & x_df['grade'].isin(range(1, 15)) &
-                    x_df['bedrooms'].isin(range(12))]
-    x_df["recently_renovated"] = np.where(x_df["yr_renovated"] >= 2013, 1, 0)
+    x_df = x_df.loc[x_df['waterfront'].isin(range(2)) &
+                    x_df['view'].isin(range(5)) &
+                    x_df['condition'].isin(range(1, 6)) &
+                    x_df['grade'].isin(range(1, 15)) &
+                    x_df['bedrooms'].isin(range(1, 12))]
     x_df[to_int_columns] = x_df[to_int_columns].astype(int)
-    x_df = x_df.drop(columns=['price', 'yr_renovated'])
+    x_df = x_df.drop(columns=to_be_removed_columns)
     return x_df if y is None else x_df, y.loc[y.index.isin(x_df.index)]
 
 
@@ -61,12 +65,13 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") ->
     output_path: str (default ".")
         Path to folder in which plots are saved
     """
+    y_std = y.std()
     for feature_name in X.columns:
         feature = X[feature_name]
-        coef = np.round(feature.cov(y) / (feature.std() * y.std()), 3)
+        coef = np.round(feature.cov(y) / (feature.std() * y_std), 3)
         px.scatter(x=feature, y=y, trendline="ols", trendline_color_override='black',
                    title=f"{coef}-Pearson Correlation between {feature_name} Values and Price",
-                   labels={"x": f"{feature_name} values", "y": "Price"})\
+                   labels={"x": f"{feature_name} values", "y": "Price"}) \
             .write_image(output_path + f"/{feature_name}_correlation.png")
 
 
@@ -75,24 +80,24 @@ if __name__ == '__main__':
     df = pd.read_csv("../datasets/house_prices.csv")
 
     # Question 1 - split data into train and test sets
-    train_X, train_y, test_X, test_y = split_train_test(df, df[['price']])
+    raw_train_X, raw_train_y, raw_test_X, raw_test_y = split_train_test(df, df[['price']])
 
     # Question 2 - Preprocessing of housing prices dataset
-    train_X, train_y = preprocess_data(train_X, train_y)
-    test_X, test_y = preprocess_data(test_X, test_y)
+    train_X, train_y = preprocess_data(raw_train_X, raw_train_y)
 
     # Question 3 - Feature evaluation with respect to response
-    feature_evaluation(train_X, train_y)
+    feature_evaluation(train_X, train_y, PATH)
 
     # Question 4 - Fit model over increasing percentages of the overall training data
     m_samples = 10
     percentage = np.arange(10, 101)
     out = np.empty((percentage.shape[0], m_samples))
+    test_X, test_y = preprocess_data(raw_test_X, raw_test_y)
     for i, f in enumerate(percentage / 100):
         for j in range(m_samples):
-            train_X_sample = train_X.sample(frac=f)
-            train_y_sample = train_y.sample(frac=f)
-            out[i, j] = LinearRegression().fit(train_X_sample, train_y_sample).loss(test_X, test_y)
+            X_sample = train_X.sample(frac=f)
+            y_sample = train_y.sample(frac=f)
+            out[i, j] = LinearRegression().fit(X_sample, y_sample).loss(test_X, test_y)
     mean = out.mean(axis=1)
     std = out.std(axis=1)
     px.scatter(x=percentage, y=mean, title="MSE as a Function of Training Set Size",
@@ -101,4 +106,4 @@ if __name__ == '__main__':
                      line=dict(color="lightgrey"), showlegend=False) \
         .add_scatter(x=percentage, y=mean + 2 * std, mode="lines",
                      line=dict(color="lightgrey"), showlegend=False, fill='tonexty') \
-        .write_image("Q4.png")
+        .write_image(PATH + r"Q4Linear.png")
